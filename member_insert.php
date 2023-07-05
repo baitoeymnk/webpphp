50c4cb85aff665e5a267457f0a73884f33eaf098
baitoeymnk <6421061010@cdti.ac.th>
2023-07-05 15:08:57 +0700

    Update

--- member_insert.php
+++ member_insert.php
@@ -1,16 +1,18 @@
-
-
 <?php
 include "connect.php";
 
-$sql = "INSERT INTO tbl_member (firstname, lastname, email,phone,photo,address)
-VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[phone]','$_POST[photo]','$_POST[address]')";
+$sql = "INSERT INTO tbl_member (firstname, lastname, email, phone, photo, address)
+VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[phone]', '$_POST[photo]', '$_POST[address]')";
 
-if ($conn->query($sql) === TRUE) {
+if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-$conn->close();
+mysqli_close($conn);
+
 ?>
+
+
+
---
+++ product_form.php
@@ -0,0 +1,19 @@
+<h2>
+    <center>
+        Product_form
+    </center>
+</h2>
+
+<form method= "post" action = "product_insert.php" enctype= "multipart/form-data">
+<center>
+    Name :
+    <input type="text" name="name" /> <br/><br/>
+    Price :
+    <input type="text" name="price" /> <br/><br/>
+    Stock :
+    <input type="text" name="stock" /> <br/><br/>
+    Description :
+    <textarea name="description" > </textarea><br/><br/>
+    <input type="submit" value="บันทึกข้อมูล" />
+<center>
+</form>
